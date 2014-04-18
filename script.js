835937501b26f8c669f03d94f99033b056e1ff36
James Milani <james.milani@gmail.com>
2014-04-18 10:46:30 -0700

    created gameboard and basic angular setup

---
+++ script.js
@@ -0,0 +1,9 @@
+function TicTacController($scope)
+{
+    $scope.rows = [ [ 'a','b','c' ], [ 'd','e', 'f' ], [ 'g', 'h', 'i' ] ];
+
+    $scope.makeMove = function(r, c) {
+        $scope.rows[r] [c] = 'X';
+    };
+}
+
