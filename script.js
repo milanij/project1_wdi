70089336c321f95195ae1e6bfa294367dc582716
James Milani <james.milani@gmail.com>
2014-04-22 19:00:33 -0700

    establishes X and Os switching

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
-function TicTacController($scope)
-{
-    $scope.rows = [ [ 'a','b','c' ], [ 'd','e', 'f' ], [ 'g', 'h', 'i' ] ];
+turnNum = 0;
 
-    $scope.makeMove = function(r, c) {
-        $scope.rows[r] [c] = 'X';
+window.onload = function( ) {
+    var whoGoesFirst = function ( ) {
+    player1 = Math.random( );
+    player2 = Math.random( );
+    if ( player1 > player2 ) {
+        alert("Player 1 goes first.");
+    } else {
+        alert("Player 2 goes first.");
+        }
+
+    };
+whoGoesFirst( );
+};
+
+
+function TicTacController($scope) {
+     $scope.columns = [ [ '','','' ], [ '','','' ], [ '', '', '' ] ];
+     $scope.isXTurn = true;
+
+     $scope.makeMove = function(c, cell) {
+
+            $scope.columns[c] [cell] = $scope.isXTurn? 'X' : 'O';
+            $scope.isXTurn = !$scope.isXTurn;
+
     };
 }
+
+
+
+
 
