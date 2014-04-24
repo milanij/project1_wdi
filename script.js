d870f9bc2632ccafeb7ced730ea68bd63f868243
James Milani <james.milani@gmail.com>
2014-04-23 20:00:19 -0700

    gets the game logic started and win conditions being checked after 5 turns

--- script.js
+++ script.js
@@ -1,53 +1,69 @@
-turnNum = 0;
-
 window.onload = function( ) {
     var whoGoesFirst = function ( ) {
     player1 = Math.random( );
     player2 = Math.random( );
     if ( player1 > player2 ) {
-        alert("Player 1 goes first.");
+        console.log("Player 1 goes first.");
     } else {
-        alert("Player 2 goes first.");
+        console.log("Player 2 goes first.");
         }
 
     };
 whoGoesFirst( );
 };
 
+var turnNum = 0;
 
 function TicTacController($scope) {
      $scope.cells = [' ',' ',' ',' ',' ',' ',' ',' ',' '];
      $scope.isXTurn = true;
+     $scope.isCellFilled = false;
 
-     $scope.makeMove = function(c) {
 
-            $scope.cells[c] = $scope.isXTurn? 'X' : 'O';
+     $scope.makeMove = function(u) {
+
+            $scope.cells[u] = $scope.isXTurn? 'X' : 'O';
             $scope.isXTurn = !$scope.isXTurn;
-
-            $scope.storeVar = function(c) {
-                    for
-                    xArray[i] = c
-
-            }
-
-            var xArray = [ ];
-            var yArray = [ ];
-
-
+            turnNum ++;
+            console.log("Turn Number " + turnNum);
+            console.log($scope.cells);
+           $scope.winCheck( );
     };
 
-    $scope.checkCell = function( ) {
+    $scope.winCheck = function( ) {
+        if( turnNum >= 5 ) {
+                console.log("winCheck is running.");
 
-    }
+                for ( i = 0; i < 9 ; i++) {
+                    if( i === 0 || 3 || 6) {
+                        $scope.cells[i]
+                    }
+                }
 
 
 
 
-
-
-
+        }
+    };
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
